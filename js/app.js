e815dedb267a8b683d6d3970a162f78ce6bb2ee6
Kristen Simmons <kristen_simmons@ymail.com>
2024-04-02 15:40:07 -0500

    working on lab 14

--- js/app.js
+++ js/app.js
@@ -19,17 +19,18 @@ AppState.prototype.instantiateProducts = function () {
 }
 
 AppState.prototype.saveToLocalStorage = function () {
-  // TODO: Fill in this instance method to save product data to local storage
+  let productJSON = JSON.stringify(this.allProducts);
+  localStorage.setItem('products', productJSON);
 }
 
 AppState.prototype.loadItems = function () {
-
-  // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
-
-  this.instantiateProducts();
-
+  let productJSON = localStorage.getItem('products');
+  if (productJSON) {
+    this.allProducts = JSON.parse(productJSON);
+  } else {
+    this.instantiateProducts();
+  }
 }
-
 
 function Product(name, fileExtension = 'jpg') {
   this.name = name;
--- js/chart.js
+++ js/chart.js
@@ -10,7 +10,71 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
+
 function renderChart() {
-}
+  let appState = new AppState();
+  appState.loadItems();
+  let labels = [];
+  let voteData = [];
+  let viewData = [];
+
+  for (let i = 0; i < appState.allProducts.length; i++) {
+    let product = appState.allProducts[i];
+    labels.push(product.name);
+    voteData.push(product.timesClicked);
+    viewData.push(product.timesShown);
+  }
+  let dataObject = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Vote Data',
+
+        data: voteData,
+        borderWidth: 2,
+        backgroundColor: 'green',
+        borderColor: 'black',
+      },
+      {
+        label: 'View Data',
+        data: viewData,
+        borderWidth: 2,
+        backgroundColor: 'black',
+        borderColor: 'green',
+      }
+    ]
+  };
+  let config = {
+    type: 'bar',
+    data: dataObject,
+    options: {
+      plugins: {
+        legend: {
+          display: true,
+          labels: {
+            color: 'lightgray'
+          }
+        }
+      }, 
+      
+      scales: {
+        y: {
+          ticks: {
+            color: 'lightgray',
+          }
+
+
+        },
+        x: {
+          beginAtZero: true,
+          ticks: {
+            color: 'lightgray',
+          }
+        },
+      }
+    }
+  };
+  new Chart(canvasElem, config);
+};
 
 renderChart();
